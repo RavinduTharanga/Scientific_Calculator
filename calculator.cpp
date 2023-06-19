cf111fba1ef3712ef82b0c519ba3571202041558
RavinduTharanga <113649586+RavinduTharanga@users.noreply.github.com>
2023-06-19 08:30:45 -0400

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,24 @@
+#include<iostream> 
+#include<math.h> 
+using namespace std;
+
+int main(){
+
+    float a,b,PI;
+    int c;
+    cout<<endl; 
+    cout<<"**************************** Calculator ***********************************\n"; 
+    cout<<"---------------------------------------------------\n"; 
+    cout<<"Operations\t"<<"\tTrigonometric Functions"<<"\t\tLogarithmic Functions\n"; 
+    cout<<"------------------------------------------------\n"; 
+    cout<<"1: Division\t\t"<<"7: Sin\t\t"<<"\t\t13: Log"<<endl; 
+    cout<<"2: Multiplication\t"<<"8: Cos\t\t"<<"\t\t14: Log with base 10"<<endl; 
+    cout<<"3: Subtraction\t\t"<<"9: Tan\t\t"<<endl; 
+    cout<<"4: Addition\t\t"<<"10: Inverse of Sin"<<endl; 
+    cout<<"5: Exponent\t\t"<<"11: Inverse of Cos"<<endl; 
+    cout<<"6: Square root\t\t"<<"12: Inverse of Tan"<<endl; 
+    cout<<"Enter the function that you want to perform : "; 
+    cin>>c; 
+    PI=3.14159265; 
+
+}
